2ab7d2a51f177c14fc0f7a1965e01c8d84525c8f
Dwight Ledet <dwightledet@gmail.com>
2020-11-12 07:36:48 +0100

    git ignore

--- meessite/settings.py
+++ meessite/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '************************************************'
+SECRET_KEY = 'yj7(2&(c8ukgtb+14%)-^)j%nlyxn(xpq*dlv31c_m#)+3%y%m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
